8816c94855d6aa1acba5bd36978e7acc28a449bd
Kolja Wilcke <kolja@apfel.de>
2024-04-05 19:02:54 +0200

    v0.1.4

--- Formula/loriini.rb
+++ Formula/loriini.rb
@@ -1,7 +1,7 @@
 class Loriini < Formula
   desc "A commandline color picker"
   homepage "https://github.com/kolja/loriini"
-  url "https://github.com/kolja/loriini/releases/download/v0.1.2/loriini-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/kolja/loriini/releases/download/v0.1.4/loriini-x86_64-apple-darwin.tar.gz"
   sha256 "9d57b73857cfb7f4e7af0af9754ba6e90e7dfdddca68a8e0a2a836fc1c2d16aa"
   license "MIT"
 
